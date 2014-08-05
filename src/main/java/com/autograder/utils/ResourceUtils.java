7cd8b8ab3e6193073626af138ceee26f09cbe0d3
Wai Ho Leung <whleung@live.com>
2014-08-05 13:15:30 -0700

    Added utils for reading resources.

---
+++ src/main/java/com/autograder/utils/ResourceUtils.java
@@ -0,0 +1,40 @@
+package com.autograder.utils;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ResourceUtils {
+    public static String readString(Class<?> clazz, String name) throws IOException {
+        return readString(clazz, name, "UTF-8");
+    }
+
+    public static String readString(Class<?> clazz, String name, String encoding) throws IOException {
+        URI uri;
+
+        try {
+            uri = clazz.getResource(name).toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        byte[] bytes = Files.readAllBytes(Paths.get(uri));
+
+        return new String(bytes, encoding);
+    }
+
+    public static int readInteger(Class<?> clazz, String name) throws IOException {
+        return Integer.parseInt(readString(clazz, name));
+    }
+
+    public static <V> V readJsonAs(Class<?> clazz, String name, Class<V> classOfT) throws IOException {
+        return GsonUtils.fromJson(readString(clazz, name), classOfT);
+    }
+
+    public static <V> V readJsonAs(Class<?> clazz, String name, Type typeOfT) throws IOException {
+        return GsonUtils.fromJson(readString(clazz, name), typeOfT);
+    }
+}
