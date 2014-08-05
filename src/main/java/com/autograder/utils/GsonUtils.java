50b9a8b3bfd1dbcc46e8c1b4b04f2e9fbe19b1c6
Wai Ho Leung <whleung@live.com>
2014-08-05 13:15:20 -0700

    Added utils for reading JSON from file.

--- src/main/java/com/autograder/utils/GsonUtils.java
+++ src/main/java/com/autograder/utils/GsonUtils.java
@@ -1,6 +1,9 @@
 package com.autograder.utils;
 
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -25,5 +28,17 @@ public class GsonUtils {
 
     public static <V> V fromJson(String json, Type classOfT) {
         return gson.fromJson(json, classOfT);
+    }
+
+    public static <V> V fromFile(String path, String encoding, Class<V> classOfT) throws IOException {
+        return gson.fromJson(readFile(path, encoding), classOfT);
+    }
+
+    public static <V> V fromFile(String path, String encoding, Type classOfT) throws IOException {
+        return gson.fromJson(readFile(path, encoding), classOfT);
+    }
+
+    private static String readFile(String path, String encoding) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)), encoding);
     }
 }
