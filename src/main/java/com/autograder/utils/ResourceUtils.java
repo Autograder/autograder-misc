aa60ccc7d993e3857e7715833550dc2803094bdf
Wai Ho Leung <whleung@live.com>
2014-08-05 20:29:36 -0700

    Fixed wrong class loader in readString.

--- src/main/java/com/autograder/utils/ResourceUtils.java
+++ src/main/java/com/autograder/utils/ResourceUtils.java
@@ -16,7 +16,7 @@ public class ResourceUtils {
         URI uri;
 
         try {
-            uri = clazz.getResource(name).toURI();
+            uri = clazz.getClassLoader().getResource(name).toURI();
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
